e74d7207fe53b62f2433fdbc77b52826c71bf784
Vadzim Ramanenka <vramanenka@gmail.com>
2016-12-09 16:59:47 +0100

    Simple web-server

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"os"
+	//"fmt"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello, world!")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", IndexEndpoint).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(
+		":8080",
+		handlers.LoggingHandler(os.Stdout, router),
+	))
+}
+
+func IndexEndpoint(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Ololo")
 }
